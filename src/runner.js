978017e2d8730eac29007f44414c1d7e91ddaf56
constdrop <iota_tz@yahoo.co.jp>
2026-09-01 21:09:01 +0900

    fix: remove ineffective RSpec exception presenter patch

--- src/runner.js
+++ src/runner.js
@@ -33,20 +33,6 @@ export function runSpecs(vm, specFiles, options = {}) {
       $stderr = StringIO.new
     ` : ''}
 
-    # Override ExceptionPresenter#read_failed_line to suppress "Unable to find ... to read failed line"
-    # when reading source files from Node.js filesystem inside WASM VM
-    module RSpec
-      module Core
-        module Formatters
-          class ExceptionPresenter
-            def read_failed_line
-              ""
-            end
-          end
-        end
-      end
-    end
-
     args = ${JSON.stringify(specArgs)}
     status = RSpec::Core::Runner.run(args)
     ${isSilent ? `
